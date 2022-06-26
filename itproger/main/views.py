c60982df3c987623d9d4dc2165f4c06d550756f6
zhili <zhil.andrey2013@yandex.ru>
2022-06-26 23:53:23 +0500

    6

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['some', 'hi', '123'],
+        'obj': {
+            'car': 'No',
+            'age': '30',
+            'hobby': 'football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
